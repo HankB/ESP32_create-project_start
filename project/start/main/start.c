18b7722882ff28f3d2e7cf0fc1ee61b79101127e
Hank Barta <hbarta@gmail.com>
2026-08-03 23:51:25 -0500

    flash LED its own task

--- project/start/main/start.c
+++ project/start/main/start.c
@@ -7,7 +7,7 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
-void app_main(void)
+static void led_blink_task(void *pvParameters)
 {
     gpio_reset_pin(CONFIG_BLINK_GPIO);
     gpio_set_direction(CONFIG_BLINK_GPIO, GPIO_MODE_OUTPUT);
@@ -18,4 +18,9 @@ void app_main(void)
         gpio_set_level(CONFIG_BLINK_GPIO, 0);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
+}
+
+void app_main(void)
+{
+    xTaskCreate(led_blink_task, "led_blink", 2048, NULL, 5, NULL);
 }
