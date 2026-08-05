7ab4a787e26b32c35e9c51696ef28a712f663d16
Hank Barta <hbarta@gmail.com>
2026-08-05 11:32:37 -0500

    uptime, rssi to periodic MQTT message

--- start/main/start.c
+++ start/main/start.c
@@ -10,14 +10,19 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
+#include "esp_timer.h"
+#include "esp_wifi.h"
+
 char topic[128];
+const char * hostname;
 
 // build a reusable topic. 
 // MUST be called after proj_wifi_init() which initializes the hostname
 // that proj_wifi_get_hostname() returns.
-static void build_topic(const char *location, const char *measurement)
+static const char * build_topic(const char *prefix, const char *location, const char *measurement)
 {
-    snprintf(topic, sizeof(topic), "HA/%s/%s/%s", proj_wifi_get_hostname(), location, measurement);
+    snprintf(topic, sizeof(topic), "%s/%s/%s/%s", prefix, hostname, location, measurement);
+    return topic;
 }
 
 static void led_blink_task(void *pvParameters)
@@ -39,8 +44,8 @@ static void led_blink_task(void *pvParameters)
 void app_main(void)
 {
     xTaskCreate(led_blink_task, "led_blink", 2048, NULL, 5, NULL);
+    hostname = generate_hostname();
 
-    // ...
     proj_wifi_init();
     if (proj_wifi_wait_connected(pdMS_TO_TICKS(10000))) {
         proj_sntp_init();
@@ -51,16 +56,20 @@ void app_main(void)
         }
         proj_mqtt_init();
         if (proj_mqtt_wait_connected(pdMS_TO_TICKS(10000))) {
-            proj_mqtt_publish("CS/ESP32", "boot", 0, false);
+            proj_mqtt_publish(build_topic("CS", "lab", "status"), "boot", 0, false);
         }
     }
     // just loop and publish periodically
-    build_topic("lab", "testing");
+    build_topic("HA", "lab", "testing");
     static size_t max_payload = 128;
     char    payload[max_payload];
     static const int publish_delay = 10 * 1000;
     while(true) {
-        snprintf(payload, sizeof(payload), "{ \"t\": %lld, \"device\":\"SNTP\" }", time(0));
+        int rssi;
+        if( ESP_OK != esp_wifi_sta_get_rssi(&rssi)) rssi=0;
+        snprintf(payload, sizeof(payload), 
+            "{ \"t\": %lld, \"uptime\":%lld, \"rssi\":%d, \"device\":\"SNTP\" }",
+            time(0), esp_timer_get_time()/1000000, rssi);
         proj_mqtt_publish(topic, payload, 0, false);
         vTaskDelay(pdMS_TO_TICKS(publish_delay));
     }
